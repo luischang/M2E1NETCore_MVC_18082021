014695c6278fb4285be1da3e42d85c1cfaded17f
Luis Chang <luis.chang@outlook.com>
2021-08-23 22:41:52 -0500

    ViewBag and ViewData

--- M2E1NETCore_MVC_Hello/Areas/Marketing/Controllers/HomeController.cs
+++ M2E1NETCore_MVC_Hello/Areas/Marketing/Controllers/HomeController.cs
@@ -17,11 +17,28 @@ namespace M2E1NETCore_MVC_Hello.Areas.Marketing.Controllers
             return View();
         }
 
-        public IActionResult ProductsWithViewModel() 
+        public IActionResult ProductsWithViewModel()
         {
             var products = GetProductsJsonLocal();
             return View(products);
         }
+
+        public IActionResult ProductsWithViewBag()
+        {
+            var products = GetProductsJsonLocal();
+            ViewBag.ProductList = products;
+            ViewBag.MyName = "Luis";
+            return View();
+        }
+
+        public IActionResult ProductsWithViewData() 
+        {
+            var products = GetProductsJsonLocal();
+            ViewData["ProductList"] = products;
+            ViewData["MyName"] = "Alberto";
+            return View();
+        }
+
 
         public List<Product> GetProductsJsonLocal()
         {
